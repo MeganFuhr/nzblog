1e0899928dad93ff556f15ce3270a2e71eb42690
Megan Fuhr <MeganFuhr@gmail.com>
2024-01-07 21:00:33 -0500

    hero.

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,23 @@ function App() {
 
   return (
     <>
-      <div className="div_nav">
-        <p className="p_title">hello, new zealand</p>
-        <p className="p_title">
-          <a className="a_title" href="#gallery">
-            View Full Gallery
-          </a>
-        </p>
-      </div>
-      <div className="wrapper">
-        <div className="div_cardContainer">
-          <div className="div_card">one</div>
-          <div className="div_card">two</div>
-          <div className="div_card">three</div>
-          <div className="div_card">four</div>
-          <div className="div_card">five</div>
-          <div className="div_card">six</div>
-          <div className="div_card">seven</div>
-          <div className="div_card">eight</div>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h3>hello, new zealand</h3>
+          <h3>Full Gallery</h3>
         </div>
       </div>
-      {/* <Footer></Footer> */}
+
+      <div className="div_cardContainer">
+        <div className="div_card">one</div>
+        <div className="div_card">two</div>
+        <div className="div_card">three</div>
+        <div className="div_card">four</div>
+        <div className="div_card">five</div>
+        <div className="div_card">six</div>
+        <div className="div_card">seven</div>
+        <div className="div_card">eight</div>
+      </div>
     </>
   );
 }
